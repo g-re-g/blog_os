17e0d6d7ccb7499f9193b6c9632ed70c2d38b862
greg <greg@greg.lol>
2023-06-16 19:19:36 -0400

    test docs

--- src/lib.rs
+++ src/lib.rs
@@ -22,14 +22,20 @@ use bootloader::{entry_point, BootInfo};
 entry_point!(test_kernel_main);
 
 pub fn init() {
+    // Disable interrupts before we do anything
+    x86_64::instructions::interrupts::disable();
+    // Setup the interrupt descriptor table to handle interrupts
     interrupts::init_idt();
+    // Setup the global descriptor table
     gdt::init();
+    //
     unsafe { interrupts::PICS.lock().initialize() };
-    // Enable interrupts on the processor so that we can receve events like
-    // key presses
+    // Enable interrupts
     x86_64::instructions::interrupts::enable();
 }
 
+// Trait for wrapping test functions to get some nice output
+// and unify them under `dyn Testable`
 pub trait Testable {
     fn run(&self);
 }
@@ -39,12 +45,16 @@ where
     T: Fn(),
 {
     fn run(&self) {
+        // print the test's nam
         serial_print!("{}...\t", core::any::type_name::<T>());
+        // run the test
         self();
+        // if we get here the test succeeded
         serial_println!("[ok]");
     }
 }
 
+// Run all the tests and exit with success if they all pass
 pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
@@ -53,10 +63,12 @@ pub fn test_runner(tests: &[&dyn Testable]) {
     exit_qemu(QemuExitCode::Success);
 }
 
+// print the test failed and the error and exit qemu
 pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
+    // Just in case qemu doesn't exit, enter infinite halt loop
     hlt_loop();
 }
 
@@ -74,6 +86,11 @@ fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
 }
 
+// qemu applies the following math to the exit code:
+// (value << 1) | 1
+// so `0x10` becomes `0x21` or 31 in decimal
+// so `0x11` becomes `0x23` or 35 in decimal
+// what is considered success his configured in cargo.toml.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
@@ -81,6 +98,8 @@ pub enum QemuExitCode {
     Failed = 0x11,
 }
 
+// Utility function to quit qemu with an exit code.
+// Configured on port 0xf4 in cargo.toml.
 pub fn exit_qemu(exit_code: QemuExitCode) {
     use x86_64::instructions::port::Port;
 
