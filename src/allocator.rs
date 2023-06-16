57e6b94c3a70a1c5081f17cbf65b022966de29f5
greg <greg@greg.lol>
2023-06-16 01:28:34 -0400

    finish the blog

---
+++ src/allocator.rs
@@ -0,0 +1,61 @@
+use alloc::alloc::{GlobalAlloc, Layout};
+use core::ptr::null_mut;
+use x86_64::{
+    structures::paging::{
+        mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
+    },
+    VirtAddr,
+};
+
+// Arbitrary starting point for the Kernel's heap. 0x4444_4444_0000 chosen
+// because it is a) unused, and b) easy to identify
+pub const HEAP_START: usize = 0x_4444_4444_0000;
+// Kernel's heap size; 100 KiB. If we need more we can always change this.
+pub const HEAP_SIZE: usize = 100 * 1024;
+
+// The #[global_allocator] attribute tells rust what static item we want to use
+// as our OS's global memory allocator. It must implement the
+// alloc::alloc::GlobalAlloc trait.
+use linked_list_allocator::LockedHeap;
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+// Initializing the Kernel's heap. We need to do this, of course, so we can
+// store data for the Kernel itself on a heap.
+pub fn init_heap(
+    mapper: &mut impl Mapper<Size4KiB>,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) -> Result<(), MapToError<Size4KiB>> {
+    // Create a PageRangeInclusive to describe our kernel's heap.
+    // https://docs.rs/x86_64/latest/x86_64/structures/paging/page/struct.PageRangeInclusive.html
+    let page_range = {
+        // The virtual address where our Kernel's heap starts.
+        let heap_start = VirtAddr::new(HEAP_START as u64);
+        // The Virtual address where it ends, end inclusive
+        let heap_end = heap_start + HEAP_SIZE - 1u64;
+        // Allign the start virtual address to a page boundary.
+        let heap_start_page = Page::<Size4KiB>::containing_address(heap_start);
+        // Align the end virtual address to a page boundary.
+        let heap_end_page = Page::<Size4KiB>::containing_address(heap_end);
+        // Construct an inclusive page range.
+        Page::<Size4KiB>::range_inclusive(heap_start_page, heap_end_page)
+    };
+
+    // Initialize all the TODO
+    for page in page_range {
+        let frame = frame_allocator
+            .allocate_frame()
+            .ok_or(MapToError::FrameAllocationFailed)?;
+        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+        // Create the page table entry for the page->frame mapping
+        unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
+    }
+
+    // Initialize our heap allocator to the correct heap location
+    // after we have initialized the page table
+    unsafe {
+        ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
+    }
+
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
+
+pub mod allocator;
+pub mod gdt;
+pub mod interrupts;
+pub mod memory;
+pub mod serial;
+pub mod task;
+pub mod vga_buffer;
+extern crate alloc;
+use core::panic::PanicInfo;
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+pub fn init() {
+    interrupts::init_idt();
+    gdt::init();
+    unsafe { interrupts::PICS.lock().initialize() };
+    // Enable interrupts on the processor so that we can receve events like
+    // key presses
+    x86_64::instructions::interrupts::enable();
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    hlt_loop();
+}
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+    init();
+    test_main();
+    hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
